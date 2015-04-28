3ea7a410792afb0c922f029756518cec76476369
serge-sans-paille <sguelton@quarkslab.com>
2015-04-28 17:19:24 +0200

    Rollback erroneous fix

--- dancedance/dancedance_term.cpp
+++ dancedance/dancedance_term.cpp
@@ -6,11 +6,17 @@
 // gets current state, save it
 // and sets the state to raw mode
 TermiOS::TermiOS() {
-    if(!tcgetattr(STDIN_FILENO, &_orig))
+    if(tcgetattr(STDIN_FILENO, &_orig))
+        std::cerr << "failed to get current mode" << std::endl;
+    else {
         cfmakeraw(&_raw);
+        if(tcsetattr(STDIN_FILENO, TCSANOW, &_raw))
+            std::cerr <<  "failed to use raw mode" << std::endl;
+    }
 }
 
 // restore saved state
 TermiOS::~TermiOS() {
-    tcsetattr(STDIN_FILENO, TCSANOW, &_orig);
+    if(tcsetattr(STDIN_FILENO, TCSANOW, &_orig))
+        std::cerr << "failed to restore raw mode" << std::endl;
 }
